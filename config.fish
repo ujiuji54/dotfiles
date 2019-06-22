a0ecd98da47b8e0d269ea6904e4422830cb9dc91
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2019-06-23 02:02:12 +0900

    add git aliases

--- config.fish
+++ config.fish
@@ -8,6 +8,8 @@ set -x PATH ~/.nodebrew/current/bin $PATH
 alias v="env nvim"
 alias vf="env nvim ~/.config/fish/config.fish"
 
+alias sf="env source ~/.config/fish/config.fish"
+alias pc="env pbcopy <"
 alias l="env ls -ltr"
 alias la="env ls -a"
 alias ll="env ls -l"
@@ -18,6 +20,10 @@ alias g='env git'
 alias gs='env git status -sb'
 alias gco='env git checkout'
 alias gp='env git pull'
+alias gd='env git diff'
+alias gl='env git log --graph --date=short --pretty=\"format:%C(yellow)%h %C(cyan)%ad %C(green)%an%Creset%x09%s %C(red)%d%Creset\"'
+alias ga='env git add'
+alias gc='env git commit -m'
 
 # rails
 alias ra="env bin/rails"
