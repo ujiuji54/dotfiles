169d80ae7833510d48963dd21541f9beef54af85
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2019-07-09 01:01:48 +0900

    makefile

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.xonshrc ~/.xonshrc
-ln -sf ~/dotfiles/config.fish ~/.config/fish/config.fish
-
